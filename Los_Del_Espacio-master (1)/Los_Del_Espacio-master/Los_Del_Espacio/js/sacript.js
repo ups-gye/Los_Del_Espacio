9e039989fc8211bc016b2eb2c9cfdf128e6acb7e
Geovanny Yusef Urgiles Miño <geovannyurgiles15@gmail.com>
2024-08-30 02:52:06 -0500

    Actualizaion300824-251AM

--- Los_Del_Espacio-master (1)/Los_Del_Espacio-master/Los_Del_Espacio/js/sacript.js
+++ Los_Del_Espacio-master (1)/Los_Del_Espacio-master/Los_Del_Espacio/js/sacript.js
@@ -30,3 +30,23 @@ window.addEventListener("load", () => {
             "<"
         );
 });
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const buttons = document.querySelectorAll('.column-button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            window.location.href = 'https://www.canva.com/design/DAGKNQQDWdg/jbxkFIHOWZs8RQehFITmcQ/edit?utm_content=DAGKNQQDWdg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton';
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const buttons = document.querySelectorAll('.column-button1');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            window.location.href = 'https://www.canva.com/design/DAGPRToK340/ohV-LJQez0Kmmb2E7VXf-Q/edit?utm_content=DAGPRToK340&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton';
+        });
+    });
+});
